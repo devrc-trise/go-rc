d5f9b17b8637a45b576af6415ec827bf50663c63
Romel Campos <rcampos@exist.com>
2015-05-08 14:12:25 +0800

    first library

---
+++ hello/hello.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/devrc-trise/go-rc/stringutil"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG, olleH") + "\n")
+}
